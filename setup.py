0985db62669badd24537c74cad0ddc8707f79dea
albieta <alba.roma@estudiantat.upc.edu>
2024-12-09 12:37:36 +0100

    release: 0.1.23

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='laia-gen-lib',
     packages=find_packages(),
-    version='0.1.22',
+    version='0.1.23',
     description='An AI application generator engine',
     author='Me',
-    install_requires=['pymongo', 'pydantic', 'datamodel-code-generator==0.25.8', 'fastapi', 'bcrypt', 'asyncinit', 'pyjwt'],
+    install_requires=['pymongo', 'pydantic==2.8.0', 'datamodel-code-generator==0.25.8', 'fastapi==0.111.0', 'bcrypt', 'asyncinit', 'pyjwt'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pymongo', 'pytest-asyncio', 'fastapi'],
     test_suite='tests',
