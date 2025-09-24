eef4a560df18486bdbc7ca2c7319bbd7a1918033
Itziar Mensa Minguito <itziar@10-192-105-27client.eduroam.upc.edu>
2025-09-24 07:52:36 +0200

    Bump version 0.2.23

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='laia-gen-lib',
     packages=find_packages(),
-    version='0.1.24',
+    version='0.2.23',
     description='An AI application generator engine',
     author='Me',
     install_requires=['pymongo', 'pydantic==2.8.0', 'datamodel-code-generator==0.25.8', 'fastapi==0.111.0', 'bcrypt', 'asyncinit', 'pyjwt'],
