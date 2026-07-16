edb91865ce133fc4e9d436ab62465de52e700332
Pedrorodriguez016 <146534432+Pedrorodriguez016@users.noreply.github.com>
2026-07-16 11:18:21 +0200

    feat: role default fields for backoffice

--- laiagenlib/Domain/LaiaUser/Role.py
+++ laiagenlib/Domain/LaiaUser/Role.py
@@ -1,4 +1,11 @@
+from pydantic import ConfigDict
 from ..LaiaBaseModel.LaiaBaseModel import LaiaBaseModel
 
 class Role(LaiaBaseModel):
     name: str
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "x-frontend-defaultFields": ["id", "name"]
+        }
+    )
